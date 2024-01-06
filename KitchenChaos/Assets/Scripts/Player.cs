7327f0f5f50a7bc472fb2ca977c7cd3cab73b163
Aditya <adityaspmahajan@gmail.com>
2024-01-06 12:25:00 +0530

    Collision Detection Completed

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -14,7 +14,32 @@ public class Player : MonoBehaviour{
 
         // Move in the direction of input vector
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        // Collision Detection With Physics.Raycast
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        /* bool canMove = !Physics.Raycast(transform.position, moveDir, playerRadius); // Raycast casues problem on the edges */
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position+Vector3.up*playerHeight, playerRadius, moveDir, moveDistance);
+
+        // Handle Diagonal Movement
+        if (!canMove) {
+            // Cannot move towards moveDir
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized; // Normalization helps to keep spped same as side even on diagonal movement
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove) {
+                // Can Move only in X
+                moveDir = moveDirX;
+            } else {
+                // Cannot move in any direction
+            }
+        }
+        // Straight movement
+        if (canMove) {
+            transform.position += moveDir * moveDistance;
+        }
+
 
         // Walking var for animator
         isWalking = moveDir != Vector3.zero;
